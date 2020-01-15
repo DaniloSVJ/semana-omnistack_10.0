c7ee1fd1d41ccaf80b9175ad6158182ebd21b607
viniciusalopes <suporte@viniciusalopes.com.br>
2020-01-15 08:55:30 -0300

    Aula02-Base do sistema (BackEnd)

---
+++ semanaomnistack10/backend/src/controllers/SearchController.js
@@ -0,0 +1,37 @@
+const Dev = require('../models/Dev')
+const parseStringAsArray = require('../utils/parseStringAsArray')
+
+module.exports = {
+    async index(request, response) {
+
+        // Buscar todos devs num raio de 10km
+        // Filtrar por tecnologia
+
+        const {
+            latitude,
+            longitude,
+            techs
+        } = request.query
+
+        const techsArray = parseStringAsArray(techs)
+
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude],
+                    },
+                    $maxDistance: 10000
+                }
+            }
+        })
+
+        return response.json({
+            devs
+        })
+    }
+}
---
+++ semanaomnistack10/backend/src/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const routes = require('./routes')
+
+const app = express()
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-cft1i.gcp.mongodb.net/week10?retryWrites=true&w=majority',{
+    useCreateIndex: true,    
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+
+app.use(express.json())
+app.use(routes)
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+
+// Query Params: req.query (Filtros, ordenação, paginação, ...)
+// Route Params: request.params (identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
+// MongoDB (Não-relacional)
+
+app.listen(3333)
