d044678be2ade20b174d30a9135efa239353d624
viniciusalopes <suporte@viniciusalopes.com.br>
2020-01-15 16:52:25 -0300

    Aula 02 finalizada

--- semanaomnistack10/backend/src/controllers/DevController.js
+++ semanaomnistack10/backend/src/controllers/DevController.js
@@ -7,35 +7,26 @@ const parseStringAsArray = require('../utils/parseStringAsArray')
 
 module.exports = {
 
-    async index(request, response) {
+    async index (request, response) { 
         const devs = await Dev.find()
-        return response.json(devs)
+        return  response.json(devs)
     },
 
-    async store(request, response) {
-        const {
-            github_username,
-            techs,
-            latitude,
-            longitude
-        } = request.body
-
-        let dev = await Dev.findOne({
-            github_username
-        })
+    async store (request, response) {
+        const { github_username , techs , latitude, longitude } = request.body
+    
+        let dev = await Dev.findOne({ github_username })
 
         if (!dev) {
             const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
-
-            const {
-                name = login, avatar_url, bio
-            } = apiResponse.data
-
+        
+            const { name = login, avatar_url, bio } = apiResponse.data
+        
             const techsArray = parseStringAsArray(techs)
-
+        
             const location = {
                 type: 'Point',
-                coordinates: [longitude, latitude]
+                coordinates: [longitude, latitude] 
             }
 
             dev = await Dev.create({
